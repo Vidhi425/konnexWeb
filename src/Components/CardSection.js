f3d08fa6868402151c7e8156866344f099bdac3f
Shashank <rajshashank2104@gmail.com>
2024-03-17 14:50:07 +0530

    fixed the CardSection

--- src/Components/CardSection.js
+++ src/Components/CardSection.js
@@ -2,11 +2,11 @@ import React from "react";
 const CardSection = ({ setDevice }) => {
   return (
     <div className="h-screen w-[65%] flex flex-col items-center justify-around p-6 ">
-      <div
-        onClick={() => setDevice("fan")}
-        className="h-[40vh] w-full flex justify-evenly items-center"
-      >
-        <div className="w-[16rem] h-[12rem] p-6 flex flex-col items-start justify-start gap-3 font-semibold border-2 border-[#DCDCE7] hover:border-[#1F3DD9] hover:scale-110 transition-transform rounded-2xl cursor-pointer trasnition-[border]">
+      <div className="h-[40vh] w-full flex justify-evenly items-center">
+        <div
+          onClick={() => setDevice("fan")}
+          className="w-[16rem] h-[12rem] p-6 flex flex-col items-start justify-start gap-3 font-semibold border-2 border-[#DCDCE7] hover:border-[#1F3DD9] hover:scale-110 transition-transform rounded-2xl cursor-pointer trasnition-[border]"
+        >
           <h2 className="text-[#081245] text-3xl">Fan</h2>
           <p className="text-[#A4A4A4] text-sm">Control the speed of the fan</p>
           <img src="./images/fan.png" alt="fan" className="h-10" />
