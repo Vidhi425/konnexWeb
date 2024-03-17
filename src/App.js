0cd069008ae3d0c7e253ac0ddc7f826298f9ca0d
Jatin Chaudhary <09jatin2005@gmail.com>
2024-03-17 12:52:15 +0530

    controls

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import "./App.css";
 import Controls from "../src/Components/FanControls";
+import CardSection from "./Components/CardSection";
+import axios from "axios";
+import { useState } from "react";
+
+axios.defaults.baseURL = "https://kodessphere-api.vercel.app";
 
 function App() {
-  return <Controls />;
+  const [device, setDevice] = useState("");
+  return (
+    <>
+      <div className="flex flex-row justify-center items-center gap-3 ">
+        <CardSection device={device} setDevice={setDevice} />
+
+        <>{device === "fan" ? <Controls /> : ""}</>
+      </div>
+    </>
+  );
 }
 
 export default App;
